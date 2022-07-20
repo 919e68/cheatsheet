0edc30d02ceaa085bbe7a7a2d101c22da6ef3013
Wilson Anciro <konekred@gmail.com>
2022-07-21 04:25:09 +0800

    up

--- linux-dev/arch/install.sh
+++ linux-dev/arch/install.sh
@@ -7,17 +7,17 @@ echo "o\n"
 
 # create partition table
 echo "n\n"
-echo "\n" # partition type   - select default
-echo "\n" # partition number - select default
-echo "\n" # first sector     - select default
-echo "\n" # last sector      - select default
+# echo "\n" # partition type   - select default
+# echo "\n" # partition number - select default
+# echo "\n" # first sector     - select default
+# echo "\n" # last sector      - select default
 
-# set partition type
-echo "t\n"
-echo "8e\n"
+# # set partition type
+# echo "t\n"
+# echo "8e\n"
 
-# activate partition
-echo "a\n"
+# # activate partition
+# echo "a\n"
 
-# finalize
-echo "w\n"
+# # finalize
+# echo "w\n"
