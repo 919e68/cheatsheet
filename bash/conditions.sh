47e2645cf6b14927f2dbce22c956bbe231d8d979
Wilson Anciro <konekred@gmail.com>
2022-08-22 17:36:04 +0800

    added bash

---
+++ bash/conditions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##############################
+#    comparison operators    #
+##############################
+# =   = -eq
+# >=	= -ge
+# >	  = -gt
+# <=	= -le
+# <	  = -lt
+# !=	= -ne
+
+
+##############################
+#      logical operators     #
+##############################
+# AND = -a
+# OR  = -o
+
+##############################
+#           if else          #
+##############################
+if [[ condition ]]; then;
+	statement
+elif [[ condition ]]; then;
+	statement
+else
+	do this by default
+fi
---
+++ bash/variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+demo_variable="an awesome value"
+echo $demo_variable
+
+exit
